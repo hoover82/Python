e05de48de2b8548a090ba67d592613ad6ee4dda6
Dan Stober <33789565+hoover82@users.noreply.github.com>
2023-01-09 19:07:10 -0700

    Create countries_typing.py
    Initial development. This script will iterate through a list of countries and find those which can be typed using only one row of keys on the qwerty keyboard

---
+++ countries_typing.py
@@ -0,0 +1,243 @@
+# Solve a puzzle - find the only country whose name ca be typed using only one row on the keyboard
+
+# https://www.worldometers.info/geography/alphabetical-list-of-countries/
+
+countries = [
+   "Afghanistan            "
+,  "Albania                "
+,  "Algeria                "
+,  "Andorra                "
+,  "Angola	               "
+,  "Antigua and Barbuda	   "
+,  "Argentina	           "
+,  "Armenia	               "
+,  "Australia	           "
+,  "Austria	               "
+,  "Azerbaijan	           "
+,  "Bahamas	               "
+,  "Bahrain	               "
+,  "Bangladesh	           "
+,  "Barbados	           "
+,  "Belarus	               "
+,  "Belgium	               "
+,  "Belize	               "
+,  "Benin	               "
+,  "Bhutan	               "
+,  "Bolivia	               "
+,  "Bosnia and Herzegovina "
+,  "Botswana	           "
+,  "Brazil	               "
+,  "Brunei	               "
+,  "Bulgaria	           "
+,  "Burkina Faso	       "
+,  "Burundi	               "
+,  "Côte d'Ivoire	       "
+,  "Cabo Verde	           "
+,  "Cambodia	           "
+,  "Cameroon	           "
+,  "Canada	               "
+,  "Central African Republic"	
+,  "Chad	                 "   
+,  "Chile	                 "   
+,  "China	                 "   
+,  "Colombia                 "   
+,  "Comoros	                 "   
+,  "Congo                    "
+,  "Costa Rica               "
+,  "Croatia	                 "
+,  "Cuba	                 "
+,  "Cyprus	                 "
+,  "Czechia                  "
+,  "Czech Republic           "
+,  "Democratic Republic of the Congo"
+,  "Denmark	              "                        
+,  "Djibouti	          "          
+,  "Dominica	          "          
+,  "Dominican Republic	  "          
+,  "Ecuador	              "          
+,  "Egypt	              "          
+,  "El Salvador	          "          
+,  "Equatorial Guinea	  "          
+,  "Eritrea	              "          
+,  "Estonia	              "          
+,  "Eswatini              "
+,  "Swaziland             "                 #same	    
+,  "Ethiopia	          "          
+,  "Fiji	              "          
+,  "Finland	              "          
+,  "France	              "          
+,  "Gabon	              "          
+,  "Gambia	              "          
+,  "Georgia	              "          
+,  "Germany	              "          
+,  "Ghana	              "          
+,  "Greece	              "          
+,  "Grenada	              "          
+,  "Guatemala	          "          
+,  "Guinea	              "          
+,  "Guinea-Bissau	      "          
+,  "Guyana	              "          
+,  "Haiti	              "          
+,  "Holy See	          "          
+,  "Honduras	          "          
+,  "Hungary	              "          
+,  "Iceland	              "          
+,  "India	              "          
+,  "Indonesia	          "          
+,  "Iran	              "          
+,  "Iraq	              "          
+,  "Ireland	              "          
+,  "Israel	              "          
+,  "Italy	              "          
+,  "Jamaica	              "          
+,  "Japan	              "          
+,  "Jordan	              "          
+,  "Kazakhstan	          "
+,  "Kenya	              "
+,  "Kiribati	          "
+,  "Kuwait	              "
+,  "Kyrgyzstan	          "
+,  "Laos	              "
+,  "Latvia	              "
+,  "Lebanon	              "
+,  "Lesotho	              "
+,  "Liberia	              "
+,  "Libya	              "
+,  "Liechtenstein	      "
+,  "Lithuania	          "
+,  "Luxembourg	          "
+,  "Madagascar	          "
+,  "Malawi	              "
+,  "Malaysia	          "
+,  "Maldives	          "
+,  "Mali	              "
+,  "Malta	              "
+,  "Marshall Islands	  "
+,  "Mauritania	          "
+,  "Mauritius	          "
+,  "Mexico	              "
+,  "Micronesia	          "
+,  "Moldova	              "
+,  "Monaco	              "
+,  "Mongolia	          "
+,  "Montenegro	          "
+,  "Morocco	              "
+,  "Mozambique	          "
+,  "Myanmar               "
+,  "Burma                 "                  #same
+,  "Namibia	              "
+,  "Nauru	              "
+,  "Nepal	              "
+,  "Netherlands	          "
+,  "New Zealand	          "
+,  "Nicaragua	          "
+,  "Niger	              "
+,  "Nigeria	              "
+,  "North Korea	          "
+,  "North Macedonia	      "
+,  "Norway	              "
+,  "Oman	              "
+,  "Pakistan	          "
+,  "Palau	              "
+,  "Palestine State	      "
+,  "Panama	              "
+,  "Papua New Guinea	  "
+,  "Paraguay	          "
+,  "Peru	              "
+,  "Philippines	          "
+,  "Poland	              "
+,  "Portugal	          "
+,  "Qatar	              "
+,  "Romania	              "
+,  "Russia	              "
+,  "Rwanda	              "
+,  "Saint Kitts and Nevis"	            
+,  "Saint Lucia	          "              
+,  "Saint Vincent and the Grenadines"	
+,  "Samoa	              "      
+,  "San Marino	          "      
+,  "Sao Tome and Principe"	    
+,  "Saudi Arabia	      "      
+,  "Senegal	              "      
+,  "Serbia	              "      
+,  "Seychelles	          "      
+,  "Sierra Leone	      "      
+,  "Singapore	          "      
+,  "Slovakia	          "      
+,  "Slovenia	          "      
+,  "Solomon Islands	      "      
+,  "Somalia	              "      
+,  "South Africa	      "      
+,  "South Korea	          "      
+,  "South Sudan	          "      
+,  "Spain	              "      
+,  "Sri Lanka	          "      
+,  "Sudan	              "      
+,  "Suriname	          "      
+,  "Sweden	              "      
+,  "Switzerland	          "      
+,  "Syria	              "      
+,  "Tajikistan	          "      
+,  "Tanzania	          "      
+,  "Thailand	          "      
+,  "Timor-Leste	          "      
+,  "Togo	              "      
+,  "Tonga	              "      
+,  "Trinidad and Tobago	  "      
+,  "Tunisia	              "      
+,  "Turkey	              "      
+,  "Turkmenistan	      "      
+,  "Tuvalu	              "      
+,  "Uganda	              "      
+,  "Ukraine	              "      
+,  "United Arab Emirates	"    
+,  "United Kingdom	      "      
+,  "United States of America"	
+,  "Uruguay	              "      
+,  "Uzbekistan	          "      
+,  "Vanuatu	              "      
+,  "Venezuela	          "      
+,  "Vietnam	              "      
+,  "Yemen	              "      
+,  "Zambia	              "      
+,  "Zimbabwe	          "      
+]
+
+
+#Replaces Ô (O with hat) with O, Removes tab, space, single quote, dash...
+def cleaned ( str ):
+   return ( str.upper().translate ( str.maketrans ( "Ô","O", " '-\t") ))
+
+
+def build_rowsmap():
+
+    keyrows = ['QWERTYUIOP','ASDFGHJKL','ZXCVBNM']
+    rowsmap = {}
+
+    for i,row in enumerate(keyrows):
+        for L in [*row]:
+            rowsmap[L] = i
+    return rowsmap;
+
+
+rowsmap = build_rowsmap()
+
+found_count = 0
+for country in countries:
+    test_row = -1
+    test=True
+    rows = []
+    for letter in [*cleaned(country) ]:
+        rows.append ( rowsmap [letter] ) 
+
+        if test_row == -1:
+            test_row = rowsmap [letter]
+        elif test_row != rowsmap [letter]:
+            test=False
+    if test:
+        print ( 'Found one ->  {}: {}'.format ( country, rows ) )
+        found_count += 1
+
+print ( 'Found count={}'.format ( found_count ))
+print ( 'Done...')
+
