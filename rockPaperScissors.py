07161fcc4d49d4e673cdd80266fd264cced58c42
Dan Stober <33789565+hoover82@users.noreply.github.com>
2023-10-26 13:07:04 -0600

    Update rockPaperScissors.py
    Refactored code. Tokenized winner logic
    
    Dan Stober, 2023-10-26

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -2,58 +2,67 @@
 
 import random
 
+def get_choice():
+
+    while True:    
+        player = None
+        try:
+            player = input ( "Enter (r)ock, (p)aper, or (s)cissors: ").lower()[0]
+            if player in choices.keys():
+                break
+        except:
+            None
+        print ( '{} is an invalid selection. Please choose r, p, or s.'.format ( player ) ) 
+
+    return player
+
+def determine_winner ( pair ):
+
+    if pair[0] == pair[1]: 
+        winner = "Tie"
+        message = "We both have {}".format ( choices[pair[0]] )
+
+    elif set(pair) == {'p','r'}:     
+            #PAPER and ROCK
+            winner = 'p'
+            message = "Paper covers Rock"
+
+    elif set(pair) == {'p','s'}:     
+            #PAPER and SCISSORS
+            winner = 's'
+            message = "Scissors cut Paper"
+
+    elif set(pair) == {'r','s'}:     
+            #ROCK and SCISSORS'
+            winner = 'r'
+            message = "Rock smashes Scissors"
+
+    return winner,message
+
 choices = {'r':'rock','p':'paper','s':'scissors'}
 options = [ choices[k] for k in choices.keys() ]
 scores = {'computer':0, 'player':0}
 
 while scores['computer']<5 and scores['player']<5:
-    computer = random.randint(0,2)
+    computer = [ k for k in choices.keys() ] [ random.randint(0,2) ]
+    player = get_choice()
+    
+    print ( 'computer: {}'.format ( computer ) ) 
+    print ( 'player: {}'.format ( player ) ) 
 
-    while 1==1:    
-        player = input ( "Enter (r)ock, (p)aper, or (s)cissors: ").lower()[0]
-        if player in choices.keys():
-            break
-        print ( '{} is an invalid selection. Please choose r, p, or s.'.format ( player ) ) 
+    winner,message = determine_winner ( list ( [computer, player] ) )
 
-    if computer == 0: 
-        if player == "s":
-            result = "I win"
-            message = "Rock beats scissors"
-            scores['computer'] += 1
-        elif player == "p":
-            result = "You win"
-            message = "Paper beats rock"
-            scores['player'] += 1
-        else:
-            result = "Tie"
-            message = "We both have rock"
-       
-    elif computer == 1: 
-        if player == "r":
-            result = "I win"
-            message = "Paper beats rock"
-            scores['computer'] += 1
-        elif player == "s":
-            result = "You win"
-            message = "Scissors beat paper"
-            scores['player'] += 1
-        else:
-            result = "Tie"
-            message = "We both have paper"
-
-    elif computer == 2: 
-        if player == "p":
-            result = "I win"
-            message = "Scissors beat paper"
-            scores['computer'] += 1
-        elif player == "r":
-            result = "You win"
-            message = "Rock beats scissors"
-            scores['player'] += 1
-        else:
-            result = "Tie"
-            message = "We both have scissors"
-
-    print ( "I have {}, you have {}.".format ( options[computer], choices[player] ) )
+    if winner == "Tie":
+        result = winner
+    elif computer == winner:
+        result = "Computer wins"
+        scores['computer'] += 1
+    elif computer == winner:
+        result = "Player wins"
+        scores['player'] += 1
+    else:
+        print ('Fatal logic error!')
+        break        
+    print ( "Computer has {}, player has {}.".format ( choices[computer], choices[player] ) )
     print ( "{}. {}.".format ( result, message ) )
-    print ( "My score: {} Your score: {}\n".format ( scores['computer'], scores['player'] ) )
+    print ( "Computer score: {} Player score: {}\n".format ( scores['computer'], scores['player'] ) )
